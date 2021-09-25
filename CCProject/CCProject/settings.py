380e1f4259ad840777d78432aa1e4af445fbb6df
Vincent Tombari <vinnietombari@gmail.com>
2021-09-25 17:25:54 -0400

    Adjust urls and allowed_hosts

--- CCProject/CCProject/settings.py
+++ CCProject/CCProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&7%i1j3qv@pv#q-84+8dn24!swk!fl@z9q9(rzjraa79rb1fxd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['SAECC.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'SAECC.pythonanywhere.com']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainPage'
 ]
 
 MIDDLEWARE = [
--- CCProject/mainPage/urls.py
+++ CCProject/mainPage/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 # URLConf
 urlpatterns = [
-  path('', views.say_hello)
+  path('main', views.say_hello)
 ]
