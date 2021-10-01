fa5a6722f35fa33900c3b98833e899bc3fe3e39b
Vincent Tombari <vinnietombari@gmail.com>
2021-10-01 01:49:28 -0400

    Added toStrings for Team, Player, and Match

--- CCProject/stats/models.py
+++ CCProject/stats/models.py
@@ -5,14 +5,29 @@ class Team(models.Model):
   wins = models.IntegerField()
   losses = models.IntegerField()
 
+  def __str__(self):
+        return self.name
+
 class Player(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
   team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
 
+  def __str__(self):
+        return self.name
+
 class Match(models.Model):
   date = models.DateField()
   home = models.ForeignKey(Team, on_delete=models.SET_NULL, related_name='home', null=True)
   away = models.ForeignKey(Team, on_delete=models.SET_NULL, related_name='away', null=True)
+  
+  # TODO: Implement scores
+  def winning_team(self):
+    return self.home
 
+  # TODO: Implement scores
+  def losing_team(self):
+    return self.away
 
+  def __str__(self):
+        return "match"
